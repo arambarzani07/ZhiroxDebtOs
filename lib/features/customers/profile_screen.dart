cc676ae3a1fde43f9cdded73316f2f4421ce3eb2
arambarzani07 <arambarzani070@gmail.com>
2026-06-20 18:30:51 +0300

    Add customer profile feature body

---
+++ lib/features/customers/profile_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../models/customer_model.dart';
+import '../../widgets/customer_balance_card.dart';
+import '../../widgets/financial_action_bar.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({
+    super.key,
+    required this.customer,
+    required this.profile,
+    required this.onDebt,
+    required this.onPayment,
+    this.loading = false,
+  });
+
+  final CustomerModel customer;
+  final CustomerProfileModel profile;
+  final VoidCallback onDebt;
+  final VoidCallback onPayment;
+  final bool loading;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(12),
+      children: [
+        CustomerBalanceCard(
+          customerName: customer.fullName,
+          phone: customer.phone,
+          currentBalance: '${profile.currentBalance} IQD',
+          creditLimit: '${profile.creditLimit} IQD',
+          trustScore: '${profile.trustScore}',
+          riskLevel: profile.riskLevel,
+          debtHealth: profile.debtHealth,
+        ),
+        const SizedBox(height: 12),
+        FinancialActionBar(
+          onDebt: onDebt,
+          onPayment: onPayment,
+          loading: loading,
+        ),
+      ],
+    );
+  }
+}
