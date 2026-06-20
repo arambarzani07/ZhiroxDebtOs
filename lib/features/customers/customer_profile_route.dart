4ded363efde4c3dc40bba7ecb55826a63f8a60ae
arambarzani07 <arambarzani070@gmail.com>
2026-06-20 23:31:04 +0300

    Add customer profile route helper

---
+++ lib/features/customers/customer_profile_route.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../app/app_flow_services.dart';
+import '../../app/app_services.dart';
+import '../../models/customer_model.dart';
+import 'customer_profile_actions.dart';
+import 'profile_loader.dart';
+
+void openCustomerProfile({
+  required BuildContext context,
+  required CustomerModel customer,
+  required AppServices services,
+}) {
+  final flows = AppFlowServices.fromClient(services.apiClient);
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (_) => ProfileLoader(
+        customer: customer,
+        service: services.customers,
+        actions: CustomerProfileActions(
+          money: flows.money,
+          approvals: flows.approvals,
+        ),
+      ),
+    ),
+  );
+}
